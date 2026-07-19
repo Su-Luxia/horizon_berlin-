e6eb417684190cc333cce3183b3ce2d56d3bba04
Su-Luxia <svitlychnala@gmail.com>
2026-07-19 11:40:57 +0200

    Fix issues

--- zelda/main.py
+++ zelda/main.py
@@ -1,36 +1,38 @@
-import pygame, sys, os
+import pygame, sys
 from setting import *
 from level import Level
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('Zelda')
-        self.clock = pygame.time.Clock()
+	def __init__(self):
 
-        self.level = Level()
+		# general setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+		pygame.display.set_caption('Zelda')
+		self.clock = pygame.time.Clock()
 
-        main_sound = pygame.mixer.Sound(os.path.join(BASE_DIR, 'main.ogg'))
-        main_sound.set_volume(0.5)
-        main_sound.play(loops = -1)
+		self.level = Level()
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_m:
-                        self.level.toggle()
-            self.screen.fill(WATER_COLOR)
-            self.level.run()
-            pygame.display.update()
-            self.clock.tick(FPS)
+		# sound 
+		main_sound = pygame.mixer.Sound('../audio/main.ogg')
+		main_sound.set_volume(0.5)
+		main_sound.play(loops = -1)
+	
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_m:
+						self.level.toggle_menu()
+
+			self.screen.fill(WATER_COLOR)
+			self.level.run()
+			pygame.display.update()
+			self.clock.tick(FPS)
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+	game = Game()
+	game.run()
